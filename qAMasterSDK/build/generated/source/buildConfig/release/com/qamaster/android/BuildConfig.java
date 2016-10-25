e99e279e0a44e512a5217440246c3f044fe9839f
fg607 <fg607@sina.com>
2016-10-25 22:13:02 +0800

    add lib

---
+++ qAMasterSDK/build/generated/source/buildConfig/release/com/qamaster/android/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.qamaster.android;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.qamaster.android";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 6;
+  public static final String VERSION_NAME = "2.3";
+}
